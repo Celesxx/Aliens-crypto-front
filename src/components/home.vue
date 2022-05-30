12d223008910bcbf14c45073551c01c3b2699153
cameron <debliquy.cameron@gmail.com>
2022-05-30 23:49:49 +0200

    feat: add animation

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,8 +4,10 @@
 
     <div class= "home-content">
 
-      <div class= "home-content-aliens-words" id="words-1"><p class="p-aliens">Bonjour humain</p></div>
-      <div class= "home-content-aliens-words" id="words-2"><p class="p-aliens">Paix humain</p></div>
+      <div class= "home-content-aliens-words" id="words-content-1"><p class="p-aliens" id="words-1">Bonjour humain</p></div>
+      <div class= "home-content-aliens-words words-margin-right" id="words-content-2"><p class="p-aliens" id="words-2">Paix humain</p></div>
+      <div class= "home-content-aliens-words" id="words-content-1 words-margin-left"><p class="p-aliens" id="words-3">Aidez nous</p></div>
+      <div class= "home-content-aliens-words" id="words-content-2"><p class="p-aliens" id="words-4">Sauvez nous</p></div>
       
     </div>
   </section>
